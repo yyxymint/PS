7e82cae51e91296d7f0a50fb6a2e33348c897d85
HwangYeChan <31719849+HwangYeChan@users.noreply.github.com>
2020-03-15 02:11:52 +0900

    Create bellman_ford.cpp

---
+++ bellman_ford.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int V,E;
+vector<pair<int,int> > adj[505];
+
+vector<int> BF(int src){
+	vector<int> upper(V+1, 987654321);
+	upper[src] = 0;
+	bool updated;
+	for(int iter=0;iter<V;iter++){
+		updated = false;
+		for(int here=1;here<=V;here++)
+			for(int i=0;i<adj[here].size();i++){
+				int there = adj[here][i].first;
+				int cost = adj[here][i].second;
+				if(upper[here]!=987654321 && upper[there] > upper[here] + cost){
+					upper[there] = upper[here] + cost;
+					updated = true;
+				}
+			}
+		if(!updated) break;
+	}
+	if(updated) upper.clear();
+	return upper; 
+}
+ 
+int main(){
+	scanf("%d%d",&V,&E);
+	while(E--){
+		int A,B,C;
+		scanf("%d %d %d",&A,&B,&C);
+		adj[A].push_back(make_pair(B,C));
+	}
+	vector<int> dist = BF(1);
+	if(!dist.size()) return !printf("-1");
+	for(int i=2;i<V+1;i++) printf("%d\n",dist[i]!=987654321?dist[i]:-1);
+}
